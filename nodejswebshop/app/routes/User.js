2040d4c32424d0c9c3f197027c190725ecab1e85
cycoborchp <joachimsimeongabriel.berchel@eduvaud.ch>
2024-05-11 16:58:22 +0200

    Remove useless file

--- nodejswebshop/app/routes/User.js
+++ nodejswebshop/app/routes/User.js
@@ -2,5 +2,5 @@ const express = require('express');
 
 const router = express.Router();
 const controller = require("../controllers/UserController");
-router.get('/', controller.get);
+router.get('/', controller.get) 
 module.exports = router; 
--- nodejswebshop/app/server.js
+++ nodejswebshop/app/server.js
@@ -1,31 +1,13 @@
 const express = require("express");
 
+
 const app = express();
-const userRouter = require('./routes/User');
-const loginRouter = require('./routes/login');
-const createAccountRouter = require('./routes/createAccount');
-const database = require('./database/database')
+const userRoute = require('./routes/User');
+app.use('/user', userRoute);
 
-//user route
-app.use('/user', userRouter);
 
-//login route
-app.use('/login', loginRouter);
-
-//create account route
-app.use('/create', createAccountRouter);
-
-const port = 8080;
-
-database.connect()
-
-//Error 404 if the URL don't exist
-app.use(({ res }) => {
-    const message = "Unable to find the requested resource! Try another URL.";
-    res.status(404).json(message);
-});
 
 // Démarrage du serveur
-app.listen(port, () => {
-    console.log('Server running on port', port);
+app.listen(8080, () => {
+    console.log('Server running on port 8080');
 });
